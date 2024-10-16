e8387da29d86a76538f8b1f0e32c1a1d5858f58d
FXZFun <58504073+FXZFun@users.noreply.github.com>
2024-10-16 11:05:59 -0500

    add unit tests for generated namespaces

--- tsts/AutoRegister.Tests/GenerationTests.cs
+++ tsts/AutoRegister.Tests/GenerationTests.cs
@@ -7,5 +7,32 @@ public partial class GenerationTests
    [Fact]
    public Task ShouldGenerateEmptyExtensions() => string.Empty.VerifyAsync();
 
+   [Fact]
+   public Task ShouldGenerateInAutoRegisterNamespace() =>
+      """
+         using AutoRegister;
+         [RegisterSingleton]
+         public class Foo { }
+         """.VerifyAsync();
+
+   [Fact]
+   public Task ShouldGenerateInGivenNamespace() =>
+      """
+         using AutoRegister;
+         namespace Bob;
+         [RegisterSingleton]
+         public class Foo { }
+         """.VerifyAsync();
+
+   [Fact]
+   public Task ShouldGenerateInAttributeNamespace() =>
+      """
+         using AutoRegister;
+         [assembly: AutoRegisterNamespace("Bar")]
+         namespace Bob;
+         [RegisterSingleton]
+         public class Foo { }
+         """.VerifyAsync();
+
    #endregion
 }
